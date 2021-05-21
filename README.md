aa23a1bc5b5801322efafbb18256b96cbd6ef7fe
jattasNI <jattasNI@users.noreply.github.com>
2021-05-21 14:34:55 -0500

    Style aggrid with custom design system provider

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Experiments with [Fast Design System](https://www.fast.design/)
 
 Initialized from Fast's [Webpack template project](https://www.fast.design/docs/integrations/webpack)
 Added [AGGrid](https://www.ag-grid.com/)
+Added [custom design system provider](https://www.fast.design/docs/design-systems/creating-a-design-system)
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { 
     FASTDesignSystemProvider, 
     FASTCard, 
-    FASTButton 
+    FASTButton
   } from '@microsoft/fast-components';
+
+import { css } from "@microsoft/fast-element";
+import {
+    DesignSystemProvider,
+    defineDesignSystemProvider,
+    DesignSystemProviderTemplate as template,
+} from "@microsoft/fast-foundation";
+
   
   /*
    * Ensure that tree-shaking doesn't remove these components from the bundle.
@@ -12,3 +20,13 @@ import {
   FASTCard;
   FASTButton;
   
+
+  @defineDesignSystemProvider({
+    name: "my-design-system-provider",
+    template,
+    styles: css`:host {     --ag-background-color: var(--background-color);
+    --ag-odd-row-background-color: var(--background-color);
+    --ag-header-background-color: var(--background-color);
+    --ag-foreground-color: var(--neutral-foreground-rest); }`
+})
+export class MyDesignSystemProvider extends FASTDesignSystemProvider {}
